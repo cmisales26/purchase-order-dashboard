5e08bb96c4266f593c2481c8bec9d704b6503eda
cmisales26 <cmi.sales26@gmail.com>
2025-09-29 14:31:18 +0530

    Add files via upload

--- PO_TAX_Streamlit.py
+++ PO_TAX_Streamlit.py
@@ -141,16 +141,16 @@ def create_invoice_pdf(invoice_data,logo_file="logo_final.jpg",stamp_file = "sta
 
     # --- Totals ---
     pdf.set_font("Helvetica", "B", 8)
-    pdf.cell(sum(col_widths[:5]), 5, "Basic Amount", border=1, align="R")
+    pdf.cell(sum(col_widths[:5]), 5, "Basic Amount", border=1, align="L")
     pdf.cell(30, 5, f"{invoice_data['totals']['basic_amount']:.2f}", border=1, ln=True, align="R")
     
-    pdf.cell(sum(col_widths[:5]), 5, "SGST @ 9%", border=1, align="R")
+    pdf.cell(sum(col_widths[:5]), 5, "SGST @ 9%", border=1, align="L")
     pdf.cell(30, 5, f"{invoice_data['totals']['sgst']:.2f}", border=1, ln=True, align="R")
     
-    pdf.cell(sum(col_widths[:5]), 5, "CGST @ 9%", border=1, align="R")
+    pdf.cell(sum(col_widths[:5]), 5, "CGST @ 9%", border=1, align="L")
     pdf.cell(30, 5, f"{invoice_data['totals']['cgst']:.2f}", border=1, ln=True, align="R")
 
-    pdf.cell(sum(col_widths[:5]), 5, "Final Amount to be Paid", border=1, align="R")
+    pdf.cell(sum(col_widths[:5]), 5, "Final Amount to be Paid", border=1, align="L")
     pdf.cell(30, 5, f"{invoice_data['totals']['final_amount']:.2f}", border=1, ln=True, align="R")
     
     # --- Amount in Words ---

ff0180b223c78c1597ccf422b46467730e35668a
cmisales26 <cmi.sales26@gmail.com>
2025-10-28 13:08:33 +0530

    Add files via upload

--- PO_TAX_QUOT.py
+++ PO_TAX_QUOT.py
@@ -1030,9 +1030,9 @@ def create_po_pdf(po_data, logo_path = "logo_final.jpg"):
     pdf.section_title("Terms & Conditions")
     pdf.set_font("Helvetica", "", 10)
     terms_lines = [
-        f"{'Taxes       ':<16}: As specified above",
-        f"{'Payment':<16}: {sanitized_payment_terms}",
-        f"{'Delivery':<16}: {sanitized_delivery_terms}"
+        f"{'Taxes':<26}: As specified above",
+        f"{'Payment':<24}: {sanitized_payment_terms}",
+        f"{'Delivery':<23}: {sanitized_delivery_terms}"
     ]
     pdf.multi_cell(0, 4, "\n".join(terms_lines))
     pdf.ln(2)

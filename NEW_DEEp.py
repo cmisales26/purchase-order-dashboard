834e6d1fb741c8726c79f6cffcdcfa541e530271
cmisales26 <cmi.sales26@gmail.com>
2025-10-29 13:26:29 +0530

    Add files via upload

--- NEW_DEEp.py
+++ NEW_DEEp.py
@@ -612,7 +612,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
 
     # Left Column (Company Details)
     pdf.multi_cell(
-        100, 5,
+        95, 6,
         "E/402, Ganesh Glory 11, Near BSNL Office, Jagatpur,\n"
         "Chenpur Road, Jagatpur Village, Ahmedabad - 382481",
         border="L"

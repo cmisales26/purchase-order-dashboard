39f4f8cd5bde40f3a5d9f0d0542b96365424761b
cmisales26 <cmi.sales26@gmail.com>
2025-11-17 13:15:19 +0530

    Add files via upload

--- NEW_DEEp.py
+++ NEW_DEEp.py
@@ -7,13 +7,7 @@ import io
 from PIL import Image
 import os
 from fpdf import FPDF, HTMLMixin
-import html
 import textwrap
-from fpdf.html import HTML2FPDF
-def unescape(self, txt):
-    return html.unescape(txt)
-
-HTML2FPDF.unescape = unescape
 
 # --- Global Data and Configuration ---
 PRODUCT_CATALOG = {
@@ -401,169 +395,103 @@ def add_page_one_intro(pdf, data):
     pdf.cell(0, 6, f"Subject :- {pdf.sanitize_text(data['subject'])}", ln=True)
     pdf.ln(5)
 
-    # --- Write paragraph with justification, bold & underline using HTML ---
-    def write_paragraph_justified(pdf, text):
-        """Write a justified paragraph with bold + underline formatting (HTML based)."""
 
-        # Terms that should be in BOLD
+    # --- Simple and Reliable Paragraph Formatting ---
+    def write_paragraph_with_formatting(pdf, text):
+        """Write paragraph with specific terms in BOLD and UNDERLINE"""
+        
+        # Terms that should be BOLD
         bold_terms = [
-            "Quotation", 
-            "CM Infotech's proposal", 
-            "CMI (CM INFOTECH)"
+            "Quotation", "CM Infotech's proposal", "CMI (CM INFOTECH)"
         ]
-
-        # Terms that should be UNDERLINED
+        
+        # Terms that should be UNDERLINED (software partnership list)
         underlined_terms = [
-            "Autodesk", "GstarCAD", "Grabert", "RuleBuddy", "CMS Intellicad",
-            "ZWCAD", "Etabs", "Trimble", "Bentley", "Solidworks", "Solid Edge",
-            "Bluebeam", "Adobe", "Microsoft", "Corel", "Chaos", "Nitro",
-            "Tally Quick Heal"
+            "Autodesk", "GstarCAD", "Grabert", "RuleBuddy", "CMS Intellicad", 
+            "ZWCAD", "Etabs", "Trimble", "Bentley", "Solidworks", "Solid Edge", 
+            "Bluebeam", "Adobe", "Microsoft", "Corel", "Chaos", "Nitro", "Tally Quick Heal"
         ]
+        
+        # Process the text
+        lines = text.split('\n')
+        
+        for line_idx, line in enumerate(lines):
+            if line.strip():
+                current_pos = 0
+                
+                # Find all formatting positions
+                format_positions = []
+                
+                # Find bold terms
+                for term in bold_terms:
+                    start = 0
+                    while True:
+                        pos = line.lower().find(term.lower(), start)
+                        if pos == -1:
+                            break
+                        format_positions.append((pos, pos + len(term), "bold"))
+                        start = pos + 1
+                
+                # Find underlined terms
+                for term in underlined_terms:
+                    start = 0
+                    while True:
+                        pos = line.lower().find(term.lower(), start)
+                        if pos == -1:
+                            break
+                        format_positions.append((pos, pos + len(term), "underline"))
+                        start = pos + 1
+                
+                # Sort by position
+                format_positions.sort()
+                
+                # Write the line with formatting
+                current_pos = 0
+                for start, end, style in format_positions:
+                    # Write text before formatting
+                    if start > current_pos:
+                        pdf.set_font("Helvetica", "", 12)
+                        pdf.write(5, line[current_pos:start])
+                    
+                    # Write formatted text
+                    formatted_text = line[start:end]
+                    if style == "bold":
+                        pdf.set_font("Helvetica", "B", 12)
+                    else:  # underline
+                        pdf.set_font("Helvetica", "BU", 12)
+                    pdf.write(5, formatted_text)
+                    
+                    current_pos = end
+                
+                # Write remaining text
+                if current_pos < len(line):
+                    pdf.set_font("Helvetica", "", 12)
+                    pdf.write(5, line[current_pos:])
+                
+                pdf.ln(5)
+        
+        pdf.ln(3)
 
-        # --- STEP 1: Escape HTML characters ---
-        html_text = (
-            text.replace("&", "&amp;")
-                .replace("<", "&lt;")
-                .replace(">", "&gt;")
-        )
-
-        # --- STEP 2: Apply Bold Formatting ---
-        for term in bold_terms:
-            html_text = html_text.replace(term, f"<b>{term}</b>")
-
-        # --- STEP 3: Apply Underline Formatting ---
-        for term in underlined_terms:
-            html_text = html_text.replace(term, f"<u>{term}</u>")
-
-        # --- STEP 4: Wrap in justified HTML paragraph ---
-        html_final = f'<p align="justify">{html_text}</p>'
-
-        # --- STEP 5: Output to PDF ---
-        pdf.write_html(html_final)
-        pdf.ln(2)
-
-
-    # -------------------------------------------------------------------------
-    # --- WRITE ALL PARAGRAPHS (INTRO + FIXED PARAGRAPHS)
-    # -------------------------------------------------------------------------
-
-    # Intro paragraph from user
+    # --- Write all paragraphs with formatting ---
+    
+    # Write the user's custom intro paragraph
     intro_text = pdf.sanitize_text(data.get("intro_paragraph", ""))
     if intro_text:
-        write_paragraph_justified(pdf, intro_text)
+        write_paragraph_with_formatting(pdf, intro_text)
 
-    # Fixed paragraphs list
+    # Fixed company introduction paragraphs
     fixed_paragraphs = [
-        "Enclosed please find our Quotation for your information and necessary action. "
-        "You're electing CM Infotech's proposal; your company is assured of our pledge to provide immediate and long-term operational advantages.",
-
+        "Enclosed please find our Quotation for your information and necessary action. You're electing CM Infotech's proposal; your company is assured of our pledge to provide immediate and long-term operational advantages.",
+        
         "CMI (CM INFOTECH) is now one of the leading IT solution providers in India, serving more than 1,000 subscribers across the India in Architecture, Construction, Geospatial, Infrastructure, Manufacturing, Multimedia and Graphic Solutions.",
-
+        
         "Our partnership with Autodesk, GstarCAD, Grabert, RuleBuddy, CMS Intellicad, ZWCAD, Etabs, Trimble, Bentley, Solidworks, Solid Edge, Bluebeam, Adobe, Microsoft, Corel, Chaos, Nitro, Tally Quick Heal and many more brings in India the best solutions for design, construction and manufacturing. We are committed to making each of our clients successful with their design technology.",
-
+        
         "As one of our privileged customers, we look forward to having you take part in our journey as we keep our eye on the future, where we will unleash ideas to create a better world!"
     ]
 
-    # Write all fixed paragraphs
     for paragraph in fixed_paragraphs:
-        write_paragraph_justified(pdf, paragraph)
-
-    # # --- Simple and Reliable Paragraph Formatting ---
-    # def write_paragraph_with_formatting(pdf, text):
-    #     """Write paragraph with specific terms in BOLD and UNDERLINE"""
-        
-    #     # Terms that should be BOLD
-    #     bold_terms = [
-    #         "Quotation", "CM Infotech's proposal", "CMI (CM INFOTECH)"
-    #     ]
-        
-    #     # Terms that should be UNDERLINED (software partnership list)
-    #     underlined_terms = [
-    #         "Autodesk", "GstarCAD", "Grabert", "RuleBuddy", "CMS Intellicad", 
-    #         "ZWCAD", "Etabs", "Trimble", "Bentley", "Solidworks", "Solid Edge", 
-    #         "Bluebeam", "Adobe", "Microsoft", "Corel", "Chaos", "Nitro", "Tally Quick Heal"
-    #     ]
-        
-    #     # Process the text
-    #     lines = text.split('\n')
-        
-    #     for line_idx, line in enumerate(lines):
-    #         if line.strip():
-    #             current_pos = 0
-                
-    #             # Find all formatting positions
-    #             format_positions = []
-                
-    #             # Find bold terms
-    #             for term in bold_terms:
-    #                 start = 0
-    #                 while True:
-    #                     pos = line.lower().find(term.lower(), start)
-    #                     if pos == -1:
-    #                         break
-    #                     format_positions.append((pos, pos + len(term), "bold"))
-    #                     start = pos + 1
-                
-    #             # Find underlined terms
-    #             for term in underlined_terms:
-    #                 start = 0
-    #                 while True:
-    #                     pos = line.lower().find(term.lower(), start)
-    #                     if pos == -1:
-    #                         break
-    #                     format_positions.append((pos, pos + len(term), "underline"))
-    #                     start = pos + 1
-                
-    #             # Sort by position
-    #             format_positions.sort()
-                
-    #             # Write the line with formatting
-    #             current_pos = 0
-    #             for start, end, style in format_positions:
-    #                 # Write text before formatting
-    #                 if start > current_pos:
-    #                     pdf.set_font("Helvetica", "", 12)
-    #                     pdf.write(5, line[current_pos:start])
-                    
-    #                 # Write formatted text
-    #                 formatted_text = line[start:end]
-    #                 if style == "bold":
-    #                     pdf.set_font("Helvetica", "B", 12)
-    #                 else:  # underline
-    #                     pdf.set_font("Helvetica", "BU", 12)
-    #                 pdf.write(5, formatted_text)
-                    
-    #                 current_pos = end
-                
-    #             # Write remaining text
-    #             if current_pos < len(line):
-    #                 pdf.set_font("Helvetica", "", 12)
-    #                 pdf.write(5, line[current_pos:])
-                
-    #             pdf.ln(5)
-        
-    #     pdf.ln(3)
-
-    # # --- Write all paragraphs with formatting ---
-    
-    # # Write the user's custom intro paragraph
-    # intro_text = pdf.sanitize_text(data.get("intro_paragraph", ""))
-    # if intro_text:
-    #     write_paragraph_with_formatting(pdf, intro_text)
-
-    # # Fixed company introduction paragraphs
-    # fixed_paragraphs = [
-    #     "Enclosed please find our Quotation for your information and necessary action. You're electing CM Infotech's proposal; your company is assured of our pledge to provide immediate and long-term operational advantages.",
-        
-    #     "CMI (CM INFOTECH) is now one of the leading IT solution providers in India, serving more than 1,000 subscribers across the India in Architecture, Construction, Geospatial, Infrastructure, Manufacturing, Multimedia and Graphic Solutions.",
-        
-    #     "Our partnership with Autodesk, GstarCAD, Grabert, RuleBuddy, CMS Intellicad, ZWCAD, Etabs, Trimble, Bentley, Solidworks, Solid Edge, Bluebeam, Adobe, Microsoft, Corel, Chaos, Nitro, Tally Quick Heal and many more brings in India the best solutions for design, construction and manufacturing. We are committed to making each of our clients successful with their design technology.",
-        
-    #     "As one of our privileged customers, we look forward to having you take part in our journey as we keep our eye on the future, where we will unleash ideas to create a better world!"
-    # ]
-
-    # for paragraph in fixed_paragraphs:
-    #     write_paragraph_with_formatting(pdf, paragraph)
+        write_paragraph_with_formatting(pdf, paragraph)
 
     # Contact Information - FIXED ALIGNMENT with clickable elements - FIXED OVERLAP
     page_width = pdf.w - 2 * pdf.l_margin

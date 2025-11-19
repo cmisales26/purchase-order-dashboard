bd35932a52fc2f73bd8469ee7574ab6eecc7e541
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 17:10:43 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -966,12 +966,12 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     
     for i, (label, value) in enumerate(buyer_lines):
         pdf.set_x(15)
-        pdf.set_font(pdf.default_font, "B", 10)
+        pdf.set_font(pdf.default_font, "B", 12)
         label_width = pdf.get_string_width(label) + 1
-        pdf.cell(label_width, 6, label, border="L", ln=0)
-        pdf.set_font(pdf.default_font, "", 10)
-        border = "" if i < len(buyer_lines) - 1 else ""
-        pdf.cell(92 - label_width, 6, value, border=border, ln=1)
+        pdf.cell(label_width, 5, label, border="L", ln=0)
+        pdf.set_font(pdf.default_font, "", 12)
+        border = "" if i < len(buyer_lines) - 1 else "B"
+        pdf.cell(92 - label_width, 5, value, border=border, ln=1)
 
     y_buyer_left_end = pdf.get_y()
     total_left_buyer_height = y_buyer_left_end - y_left_buyer_start

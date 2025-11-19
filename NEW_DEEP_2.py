09282a6637f2498969fd0cd23b1f4f0b43d083a2
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 16:15:41 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -845,7 +845,7 @@ class PDF(FPDF):
 
     def header(self):
         self.set_font(self.default_font, "B", 15)
-        self.cell(-1, 6, "TAX INVOICE", ln=True, align="L")
+        self.cell(-10, 6, "TAX INVOICE", ln=True, align="L")
         self.ln(3)
         
     def footer(self):

0cfc88c71264390f2d8879534271bd06261a69df
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 16:26:49 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -872,8 +872,8 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     # === HEADER (Vendor + Invoice Details) ===
     pdf.set_font(pdf.default_font, "B", 13)
     pdf.cell(97, 8, "CM Infotech.", border=1, ln=0)
-    pdf.cell(48, 8, "Invoice No.", border=1, ln=0, align="C")
-    pdf.cell(48, 8, "Invoice Date", border=1, ln=1, align="C")
+    pdf.cell(48, 8, "Invoice No.", border=1, ln=0, align="L")
+    pdf.cell(48, 8, "Invoice Date", border=1, ln=1, align="L")
 
     y_left_start = pdf.get_y()
 
@@ -890,7 +890,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     ]
     
     for i, (label, value) in enumerate(vendor_lines):
-        pdf.set_x(15)
+        pdf.set_x(10)
         pdf.set_font(pdf.default_font, "B", 12)
         label_width = pdf.get_string_width(label) 
         pdf.cell(label_width, 5, label, border="L", ln=0)

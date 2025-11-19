da340a82cf72f93994ebf52248658b4dde04ab56
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 13:47:50 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1292,7 +1292,7 @@ def create_po_pdf(po_data, logo_path = "logo_final.jpg"):
     # --- Vendor & Bill/Ship ---
     pdf.section_title("Vendor & Addresses")
     pdf.set_font(pdf.default_font, "", 10)
-    pdf.multi_cell(70, 5, f"{sanitized_vendor_name}\n{sanitized_vendor_address}\nKind Attend: {sanitized_vendor_contact}\nMobile: {sanitized_vendor_mobile}")
+    pdf.multi_cell(95, 5, f"{sanitized_vendor_name}\n{sanitized_vendor_address}\nKind Attend: {sanitized_vendor_contact}\nMobile: {sanitized_vendor_mobile}")
     pdf.ln(7)
     # pdf.set_xy(110, pdf.get_y() - 20)
     pdf.multi_cell(70, 5, f"Bill To: \n{sanitized_bill_to_company}\n{sanitized_bill_to_address}")

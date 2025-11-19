95f94911229eb15ab4fa3c60a950b952ce073d54
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 15:26:35 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -852,7 +852,7 @@ class PDF(FPDF):
 # --- Function to Create Invoice PDF ---
 def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="stamp.jpg"):
     pdf = PDF()
-    pdf.set_auto_page_break(auto=True, margin=10)
+    pdf.set_auto_page_break(auto=False, margin=10)
     pdf.add_page()
 
         # --- Logo on top right ---
@@ -1115,9 +1115,9 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     pdf.cell(180, 5, f"Tax Amount (in words): {invoice_data['totals']['tax_in_words']}", ln=True, border=1)
 
     # --- Reserve footer space ---
-    # needed_space = 70
-    # if pdf.get_y() + needed_space > pdf.h - pdf.b_margin:
-    #     pdf.set_y(pdf.h - pdf.b_margin - needed_space)
+    needed_space = 70
+    if pdf.get_y() + needed_space > pdf.h - pdf.b_margin:
+        pdf.set_y(pdf.h - pdf.b_margin - needed_space)
 
     # --- Bank Details & Declaration (Side by Side) ---
     pdf.set_font(pdf.default_font, "B", 10)

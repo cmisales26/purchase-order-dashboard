b9f505688ce5fc5b43f79e648ebafe68a2150187
cmisales26 <cmi.sales26@gmail.com>
2025-11-19 17:37:14 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1021,7 +1021,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
 
     # --- Item Table Header ---
     pdf.set_font(pdf.default_font, "B", 12)
-    col_widths = [15, 75, 20, 20, 24, 31]
+    col_widths = [15, 77, 22, 22, 26, 33]
     
     # Header row
     pdf.cell(col_widths[0], 6, "Sr. No.", border=1, align="C")

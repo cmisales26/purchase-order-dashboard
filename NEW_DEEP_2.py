381c008be4a42de46a0dd59bc721b00116e0abb7
cmisales26 <cmi.sales26@gmail.com>
2025-11-20 17:53:14 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1122,14 +1122,14 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     # Row 4: Terms of delivery
     pdf.set_x(105)
     pdf.set_font(pdf.default_font, "B", 12)
-    pdf.cell(48, 7, "Terms of delivery", border="LRT", ln=0)
+    pdf.cell(48, 6, "Terms of delivery", border="LRT", ln=0)
     pdf.set_font(pdf.default_font, "", 12)
-    pdf.cell(48, 7, invoice_data['invoice_details']['terms_of_delivery'], border="LRT", ln=1)
+    pdf.cell(48, 6, invoice_data['invoice_details']['terms_of_delivery'], border="LRT", ln=1)
 
 
     # Closing row
-    pdf.set_x(105)
-    pdf.cell(96, 1, "", border="LRB", ln=1)
+    # pdf.set_x(105)
+    # pdf.cell(96, 1, "", border="LRB", ln=1)
 
     # --- Item Table Header ---
     pdf.set_font(pdf.default_font, "B", 12)

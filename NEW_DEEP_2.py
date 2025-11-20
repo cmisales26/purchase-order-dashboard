70813e375746efef48341025e7f9f83b9abee061
cmisales26 <cmi.sales26@gmail.com>
2025-11-20 14:00:59 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1177,7 +1177,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     x_left = pdf.get_x()
 
     # Left cell (Bank) with border
-    pdf.multi_cell(94, 5, bank_text, border=1)
+    pdf.multi_cell(94, 8, bank_text, border=1)
     y_after_left = pdf.get_y()
     
     # Right cell (Declaration) with border

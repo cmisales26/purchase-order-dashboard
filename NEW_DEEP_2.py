0591cef3891ecaeab64878d7be5fb6bc0301a3f2
cmisales26 <cmi.sales26@gmail.com>
2025-11-20 15:23:58 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -909,8 +909,8 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
 
     # === HEADER (Vendor + Invoice Details) ===
     pdf.set_font(pdf.default_font, "B", 13)
-    pdf.cell(94, 8, "CM Infotech.", border=1, ln=0)
-    pdf.cell(45, 8, "Invoice No.", border=1, ln=0, align="L")
+    pdf.cell(95, 8, "CM Infotech.", border=1, ln=0)
+    pdf.cell(48, 8, "Invoice No.", border=1, ln=0, align="L")
     pdf.cell(48, 8, "Invoice Date", border=1, ln=1, align="L")
 
     y_left_start = pdf.get_y()

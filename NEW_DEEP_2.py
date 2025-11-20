2aaf3161d1731c40e82fd80866dfbd3e0a5bda93
cmisales26 <cmi.sales26@gmail.com>
2025-11-20 14:04:41 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1159,7 +1159,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     # --- Bank Details & Declaration (Side by Side) ---
     # pdf.ln(2)
     pdf.set_font(pdf.default_font, "B", 10)
-    pdf.cell(94, 4, "Company's Bank Details", ln=0,border=1)
+    pdf.cell(94, 6, "Company's Bank Details", ln=0,border=1)
     pdf.cell(94, 6, "Declaration:", ln=1,border=1)
 
     pdf.set_font(pdf.default_font, "", 10)
@@ -1177,7 +1177,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     x_left = pdf.get_x()
 
     # Left cell (Bank) with border
-    pdf.multi_cell(94, 5, bank_text, border=1)
+    pdf.multi_cell(94, 6, bank_text, border=1)
     y_after_left = pdf.get_y()
     
     # Right cell (Declaration) with border
@@ -1190,7 +1190,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     pdf.set_y(max_y)
 
     # --- Signature ---
-    pdf.ln(3)
+    pdf.ln(2)
     pdf.set_font(pdf.default_font, "B", 10)
     pdf.cell(0, 6, "For CM Infotech.", ln=True, align="R")
 

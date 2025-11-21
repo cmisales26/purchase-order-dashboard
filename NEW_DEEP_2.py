f2aeb9c1eac47e09ad6c768655095c7a862d666c
cmisales26 <cmi.sales26@gmail.com>
2025-11-21 14:35:29 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1765,7 +1765,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     pdf.set_y(-30)  # Position from bottom
     
     # Horizontal line
-    pdf.line(pdf.l_margin, pdf.get_y(), pdf.w - pdf.r_margin, pdf.get_y())
+    pdf.line(pdf.l_margin, pdf.get_y(), pdf.w - 10, pdf.get_y())
     pdf.ln(2)
     
     # Footer content

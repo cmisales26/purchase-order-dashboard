2b474c4dd294585d08db71bf1b98d11d1cadc873
cmisales26 <cmi.sales26@gmail.com>
2025-11-22 15:23:17 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1008,7 +1008,7 @@ class PDF(FPDF):
         
     def footer(self):
         # Position at 1.5 cm from bottom
-        self.set_y(-20)
+        self.set_y(-15)
         
         # Footer content
         self.set_font(self.default_font, "I", 10)

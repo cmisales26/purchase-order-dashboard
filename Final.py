334a7145d50df23278afc4558e48c11f861a1709
cmisales26 <cmi.sales26@gmail.com>
2025-11-24 15:10:21 +0530

    Add files via upload

--- Final.py
+++ Final.py
@@ -1510,7 +1510,7 @@ class PO_PDF(FPDF):
                 # (self.logo_path, x=155, y=8, w=50)
                 # (self.logo_path, x=160, y=5.5, w=45,link=self.website_url)
                 # self.image(self.logo_path, x=150, y=10, w=40)
-            self.ln(4)
+            self.ln(2)
             # Title
             self.set_font(self.default_font, "BU", 15)
             self.cell(0, 15, "PURCHASE ORDER", ln=True, align="C")

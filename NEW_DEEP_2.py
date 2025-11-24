fa7702437ca9b6ea31af1426f4e0712942ce0f19
cmisales26 <cmi.sales26@gmail.com>
2025-11-24 18:14:57 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1079,7 +1079,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
         label_width = pdf.get_string_width(label) 
         pdf.cell(label_width, 6, label, border="L", ln=0)
         pdf.set_font(pdf.default_font, "", 12)
-        border = "RL" if i < len(vendor_lines) - 1 else "RL"
+        border = "R" if i < len(vendor_lines) - 1 else "R"
         pdf.cell(95 - label_width, 6, value, border=border, ln=1)
 
     y_left_end = pdf.get_y()

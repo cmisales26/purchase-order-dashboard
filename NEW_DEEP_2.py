9b9fe20f79d8eefaa2c8b8b25e9090054006e395
cmisales26 <cmi.sales26@gmail.com>
2025-11-24 13:25:50 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1118,9 +1118,9 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
 
     # === BUYER SECTION ===
     pdf.set_font(pdf.default_font, "B", 12)
-    pdf.cell(95, 6, "Buyer", border="LT", ln=0)
-    pdf.cell(48, 6, "Buyer's Order No.", border=1, ln=0, align="L")
-    pdf.cell(48, 6, "Buyer's Order Date", border=1, ln=1, align="L")
+    pdf.cell(95, 5, "Buyer", border="LT", ln=0)
+    pdf.cell(48, 5, "Buyer's Order No.", border=1, ln=0, align="L")
+    pdf.cell(48, 5, "Buyer's Order Date", border=1, ln=1, align="L")
 
     y_buyer_start = pdf.get_y()
 

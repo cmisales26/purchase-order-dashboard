4dc70cf169f806be777dcfdbda018e50f52da3d5
cmisales26 <cmi.sales26@gmail.com>
2025-11-27 14:23:38 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1660,7 +1660,7 @@ class PO_PDF(FPDF):
 
     def footer(self):
         # Position from bottom (same as invoice)
-        self.set_y(-10)
+        self.set_y(-12)
         
         # Horizontal line
         # self.line(self.l_margin, self.get_y(), self.w - self.r_margin, self.get_y())

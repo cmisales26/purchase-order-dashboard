fbfe3afe7d9e0a753fd85dc6f68285e79fd5b1a9
cmisales26 <cmi.sales26@gmail.com>
2025-11-27 14:16:48 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1077,7 +1077,7 @@ class PDF(FPDF):
             except Exception as e:
                 # You can add a warning here if needed, but don't show in header
                 pass
-        self.ln(2)
+        self.ln(3)
         self.set_font(self.default_font, "B", 15)
         self.cell(0, 6, "TAX INVOICE", ln=True, align="C")
         self.ln(5)

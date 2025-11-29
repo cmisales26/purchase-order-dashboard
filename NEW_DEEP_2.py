be0358b61728369ea1dd9d4dde4cbec6cf68d8d3
cmisales26 <cmi.sales26@gmail.com>
2025-11-29 15:09:15 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -124,8 +124,8 @@ VENDOR_DATABASE = {
 
 # End User Database - You can expand this with more end users
 END_USER_DATABASE = {
-    "Baldridge & Associates Structural Engineering Private Limited": {
-        "address": "406 Sakar East B/5 Gurunanak School, \n40mt Tarsali danteshwar Ring Road, Vadodara",
+    "Baldridge & Associates Structural \nEngineering Private Limited": {
+        "address": "406 Sakar East B/5 Gurunanak School, 40mt Tarsali danteshwar Ring Road, Vadodara",
         "contact": "Mr. Divyesh Mistry ",
         "mobile": "98987 91813",
         "email": "dmistry@baseww.com",

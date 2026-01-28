1fdcc3355bb027368e4680ace44a3732f26297c9
cmisales26 <cmi.sales26@gmail.com>
2026-01-28 23:46:34 +0530

    Add files via upload

--- NEW_DEEP_2.py
+++ NEW_DEEP_2.py
@@ -1527,7 +1527,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
 
     # Left side - Declaration box
     pdf.set_font(pdf.default_font, "B", 10)
-    pdf.cell(95, 6, "Declaration:", border="LRT", ln=0, align="C")
+    pdf.cell(95, 6, "Terms:", border="LRTB", ln=0, align="L")
 
     # Right side - Our Company Signature box header
     pdf.cell(96, 6, "For CM INFOTECH.", border="LRT", ln=1, align="C")
@@ -1537,7 +1537,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     right_signature_box_height = 33
 
     # Left box - Declaration terms (as one paragraph, left aligned)
-    pdf.set_font(pdf.default_font, "", 8)
+    pdf.set_font(pdf.default_font, "", 10)
     pdf.set_text_color(0, 0, 0)
 
     # Combine all terms into one paragraph
@@ -1548,7 +1548,7 @@ def create_invoice_pdf(invoice_data, logo_file="logo_final.jpg", stamp_file="sta
     pdf.cell(95, left_box_height, "", border="LRB", ln=0)
 
     # Write declaration text (same as before)
-    text_x = 12
+    text_x = 10
     text_y = y_signature_start + 10
     line_height = 4
     max_width = 91
